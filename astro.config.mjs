0a61bda7d54873e276ee102190a6488bb64aa944
Heksu_Howaito <dev@fakhrif.my.id>
2026-07-30 08:08:45 +0700

    Add Cloudflare adapter and Wrangler configuration for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://segara-city-project.fakhri6203.workers.dev",
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
